e8bdec7d7103feced6deae193c9b2bc3eaba53aa
joaopedrosduarte <jpds@cesar.school>
2022-03-05 10:24:56 -0300

    Adicionando conteudo

---
+++ curso/basico/condições1.js
@@ -0,0 +1,9 @@
+var vel = 15
+
+if (vel > 14){
+    console.log(`Você esta acima da velocidade permitida pela lei 14Km/h / Sua velocidade : ${vel}`)
+} else {
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança!`)
