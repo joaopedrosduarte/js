1f16ce6dbf8aba12b2fdd32217ea12037937413d
joaopedrosduarte <jpds@cesar.school>
2022-03-05 11:40:17 -0300

    Projeto 1

--- curso/basico/condições1.js
+++ curso/basico/condições1.js
@@ -1,4 +1,6 @@
 var vel = 15
+var data = new Date()
+var hora = data.getDay()
 
 if (vel > 14){
     console.log(`Você esta acima da velocidade permitida pela lei 14Km/h / Sua velocidade : ${vel}`)
@@ -7,3 +9,38 @@ if (vel > 14){
 }
 
 console.log(`Dirija sempre com o cinto de segurança!`)
+
+console.log('\nDia da semana : ')
+switch (hora) {
+    case 0:
+        console.log('Domingo')
+        break
+    
+    case 1:
+        console.log('Segunda')
+        break
+
+    case 2:
+        console.log('Terça')
+        break 
+
+    case 3:
+        console.log('Quarta')
+        break
+
+    case 4:
+        console.log('Quinta')
+        break
+
+    case 5:
+        console.log('Sexta')
+        break
+
+    case 6:
+        console.log('Sabado')
+        break
+
+    default:
+        console.log('Error')
+        break
+}
