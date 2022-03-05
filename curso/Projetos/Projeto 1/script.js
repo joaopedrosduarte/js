d74baf8b7510409333a2aa26a542b65594db0e15
joaopedrosduarte <jpds@cesar.school>
2022-03-05 11:45:50 -0300

    Projeto 1 Update

--- curso/Projetos/Projeto 1/script.js
+++ curso/Projetos/Projeto 1/script.js
@@ -3,19 +3,21 @@ var hora = data.getHours()
 var time = document.querySelector('#time')
 var img = document.querySelector('#img')
 
-time.innerHTML = `Agora são ${hora} horas.`
+function run(){
+    time.innerHTML = `Agora são ${hora} horas.`
 
-if (hora >= 0 && hora <= 12) {
-    img.src = './public/AmanhacerC.png'
-    document.body.style.background = '#7EA9AD'
-} else if (hora > 12 && hora < 18){
-    img.src = './public/EntardecerC.png'
-    document.body.style.background = '#A2555E'
-    document.body.style.color = 'white'
-    time.style.color = 'black'
-} else {
-    img.src = './public/AnoitecerC.png'
-    document.body.style.background = '#3E4364'
-    document.body.style.color = 'white'
-    time.style.color = 'black'
+    if (hora >= 0 && hora <= 12) {
+        img.src = './public/AmanhacerC.png'
+        document.body.style.background = '#7EA9AD'
+    } else if (hora > 12 && hora < 18){
+        img.src = './public/EntardecerC.png'
+        document.body.style.background = '#A2555E'
+        document.body.style.color = 'white'
+        time.style.color = 'black'
+    } else {
+        img.src = './public/AnoitecerC.png'
+        document.body.style.background = '#3E4364'
+        document.body.style.color = 'white'
+        time.style.color = 'black'
+    }
 }
